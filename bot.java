b61a65f202683729e0b5067400268541affdd6fa
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-22 16:32:13 +0530

    data to be collected from user

--- bot.java
+++ bot.java
@@ -1,6 +1,49 @@
 public class bot {
-    public static void mani()
-    {
+     
+    public static void main(String[] args) {
+
+
+        double risk = 0; // 0 - 100 risk factor
+
+        //personal details
+        String username;
+        String userid;
+        String DOB;
+        String sex;
+        double height = 175;  
+        double weight = 70;   
+        double BMI = weight / Math.pow(height / 100, 2); 
+        short age;
+        String country;
+        String state;
+        String district;
+
+        //lifestyle
+       int  smoking ; // 0-5 consumption level
+       int alcohol ; // 0-5 consumption level
+       int drug ; // 0-5 times consumption level
+       String exercise ; // daily (low,moderate,high) weekends (low,moderate,high)
         
+
+
+       //Cardiovascular Health Assessment
+       boolean heartattack; 
+       short  stroke; // 0 times or more
+       int HBP; // high blood pressure , if yes then enter the last reading 
+       int LBP; // high blood pressure , if yes then enter the last reading 
+       int cholesterol_levels ;
+
+       int chestpainfreq;
+        double chestpainfreq_duration; //min
+        
+
+
+        
+                    
+
+
+
+
+       
     }
 }
