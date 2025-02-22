d531e88f9cd53d30548d9e50e866d0cc402dbb44
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-22 22:52:57 +0530

    making connection with database

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner; // for input 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public class Main {
+
+    private static final String Url = "jdbc:mysql://127.0.0.1:3306/"; 
+
+    private static final String username = "root";
+
+    private static final String password = "Kash@17022005";
+
     public static void main(String [] args){
         userdata user = new userdata();
-        user.user_input();
+        user.user_input(); // used to take inputs from user 
+
+
+
+
     }
 }
