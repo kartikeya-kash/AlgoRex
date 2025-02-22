65ab813e16cac8baf881501e17f3f481a04ef6f1
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-22 17:22:16 +0530

    added lifestyle header in table

--- database.sql
+++ database.sql
@@ -14,3 +14,10 @@ CREATE TABLE HealthProfile (
     country VARCHAR(100),
     state VARCHAR(100),
     district VARCHAR(100),
+  -- Lifestyle
+    smoking TINYINT CHECK (smoking BETWEEN 0 AND 5),
+    alcohol TINYINT CHECK (alcohol BETWEEN 0 AND 5),
+    drug TINYINT CHECK (drug BETWEEN 0 AND 5),
+    exercise ENUM('daily_low', 'daily_moderate', 'daily_high', 
+                  'weekends_low', 'weekends_moderate', 'weekends_high', 'none'),
+	
