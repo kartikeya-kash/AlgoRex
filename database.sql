18454a40ba135a5915324130b06fee711df61fd2
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-22 17:14:42 +0530

    create a table

--- database.sql
+++ database.sql
@@ -1,3 +1,16 @@
 create database hackx;
 use hackx;
 
+CREATE TABLE HealthProfile (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100),
+    userid VARCHAR(50),
+    DOB DATE,
+    sex ENUM('Male', 'Female', 'Other'),
+    height DOUBLE,
+    weight DOUBLE,
+    BMI DOUBLE GENERATED ALWAYS AS (weight / POW(height / 100, 2)) STORED,
+    age SMALLINT,
+    country VARCHAR(100),
+    state VARCHAR(100),
+    district VARCHAR(100),
