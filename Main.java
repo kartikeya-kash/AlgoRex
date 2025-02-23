739e208c9fc71ee658a8829a84e6a12e55489183
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-23 05:35:55 +0530

    updated the conenction

--- Main.java
+++ Main.java
@@ -23,16 +23,16 @@ public class Main {
                         "Audit logs should record user consent, ensuring compliance with data privacy regulations.");
 
                         int consent ; // 0 is for No and 1 is for Yes
+                        System.out.println("Enter 0 for no and enter 1 for accepting ");
                         Scanner input = new Scanner(System.in);
                         consent = input.nextInt();
-        if(consent != 1)
+
+        if(consent != 1) // will exit the function 
         {
             System.out.println("Thank you for using VMEA by AlgoRex");
             return ;
         }
 
-
-        
 
         try{
             Class.forName("com.mysql.cj.jdbc.Driver"); // load all drivers
@@ -57,9 +57,53 @@ public class Main {
                        
                         break;
                     case 2:
-                    userdata user = new userdata();
-                    user.user_input(); // used to take inputs from user
+                    { 
+                        userdata user = new userdata();
+                        user.user_input(); // used to take inputs from user
+                        
+                        try {
+                
+                            String sql = "INSERT INTO userdata (username, userid, DOB, sex, height, weight, age, country, state, district, " +
+                            "smoking, alcohol, drug, exercise, heartattack, stroke, HBP, LBP, cholesterol_levels, " +
+                            "chestpainfreq, chestpainfreq_duration, breath_shortness, diabetes, dib_type, l_sugar_level, medication, " +
+                            "hasHypothyroidism, hasHyperthyroidism, takesThyroidMedications, hasWeightLossHistory, hadBariatricSurgery, " +
+                            "hdl, ldl, triglycerides, hasLiverDisease, hasAlcoholLiverDamage, hasKidneyDisease, hadKidneyStones, hadDialysis, takesKidneyAffectingMeds, " +
+                            "hasAsthma, hasCOPD, hasSmokingLungDamage, hasSleepApnea, usesCPAP, hasFrequentLungInfections, hadStroke, hadBrainInjury, " +
+                            "hasDepression, hasAnxiety, hasBipolarDisorder, takesPsychiatricMeds, hasMemoryLoss, hasDementiaSymptoms, hadCancer, cancerStage, " +
+                            "hadCancerTreatment, familyHasHeartDisease, familyHasDiabetes, familyHasCancer, hasGeneticDisorders, hadRecentBloodTests, hadMRI, " +
+                            "hadCTScan, hadXRay, hadBiopsy, takesPrescriptionMeds, takesOTCMeds, hasDrugAllergies, hadCovidVaccine, hadFluVaccine, hadHepatitisVaccine) " +
+                            "VALUES ('" + user.username + "', '" + user.userid + "', '" + user.DOB + "', '" + user.sex + "', '" + user.height + "', '" + user.weight + "', " +
+                            "'" + user.age + "', '" + user.country + "', '" + user.state + "', '" + user.district + "', '" + user.smoking + "', '" + user.alcohol + "', " +
+                            "'" + user.drug + "', '" + user.exercise + "', '" + user.heartattack + "', '" + user.stroke + "', '" + user.HBP + "', '" + user.LBP + "', " +
+                            "'" + user.cholesterol_levels + "', '" + user.chestpainfreq + "', '" + user.chestpainfreq_duration + "', '" + user.breath_shortness + "', " +
+                            "'" + user.diabetes + "', '" + user.dib_type + "', '" + user.l_sugar_level + "', '" + user.medication + "', '" + user.hasHypothyroidism + "', " +
+                            "'" + user.hasHyperthyroidism + "', '" + user.takesThyroidMedications + "', '" + user.hasWeightLossHistory + "', '" + user.hadBariatricSurgery + "', " +
+                            "'" + user.hdl + "', '" + user.ldl + "', '" + user.triglycerides + "', '" + user.hasLiverDisease + "', '" + user.hasAlcoholLiverDamage + "', " +
+                            "'" + user.hasKidneyDisease + "', '" + user.hadKidneyStones + "', '" + user.hadDialysis + "', '" + user.takesKidneyAffectingMeds + "', " +
+                            "'" + user.hasAsthma + "', '" + user.hasCOPD + "', '" + user.hasSmokingLungDamage + "', '" + user.hasSleepApnea + "', '" + user.usesCPAP + "', " +
+                            "'" + user.hasFrequentLungInfections + "', '" + user.hadStroke + "', '" + user.hadBrainInjury + "', '" + user.hasDepression + "', " +
+                            "'" + user.hasAnxiety + "', '" + user.hasBipolarDisorder + "', '" + user.takesPsychiatricMeds + "', '" + user.hasMemoryLoss + "', " +
+                            "'" + user.hasDementiaSymptoms + "', '" + user.hadCancer + "', '" + user.cancerStage + "', '" + user.hadCancerTreatment + "', " +
+                            "'" + user.familyHasHeartDisease + "', '" + user.familyHasDiabetes + "', '" + user.familyHasCancer + "', '" + user.hasGeneticDisorders + "', " +
+                            "'" + user.hadRecentBloodTests + "', '" + user.hadMRI + "', '" + user.hadCTScan + "', '" + user.hadXRay + "', '" + user.hadBiopsy + "', " +
+                            "'" + user.takesPrescriptionMeds + "', '" + user.takesOTCMeds + "', '" + user.hasDrugAllergies + "', '" + user.hadCovidVaccine + "', " +
+                            "'" + user.hadFluVaccine + "', '" + user.hadHepatitisVaccine + "');";
+                
+                            try (Statement statement = connection.createStatement()) {
+                                int affectedRows = statement.executeUpdate(sql);
+                
+                                if (affectedRows > 0) {
+                                    System.out.println("Data receved!");
+                                } else {
+                                    System.out.println("Data Not receved!");
+                                }
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
 
+
+                    }
                         break;
                     case 0:
                         exit(); //exit function
@@ -92,7 +136,10 @@ public class Main {
             i--;
         }
         System.out.println();
-        System.out.println("ThankYou For Using Hotel Reservation System!!!");
+        System.out.println("ThankYou For Using  Virtual Medical Examiner Assistant (VMEA)!!!");
+        
     }
     
 }
+
+
